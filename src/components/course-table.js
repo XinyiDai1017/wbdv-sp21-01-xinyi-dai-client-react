7b3e49f8df34376c0044b9a1e2cda1407be8df19
XinyiDai1017 <daixinyi96@gmail.com>
2021-02-19 00:33:58 -0800

    add table,grid,card

---
+++ src/components/course-table.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import CourseRow from "./course-row";
+import {Link} from "react-router-dom";
+
+export default class CourseTable extends
+    React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th >Title</th>
+                            <th >Owned by</th>
+                            <th >Last modified</th>
+                            <th className="float-right">
+                                <i className="fas fa-folder"></i>
+                                &nbsp;
+                                <i className="fas fa-sort-alpha-down"></i>
+                                &nbsp;
+                                <Link to="/courses/grid">
+                                    <i className="fas fa-th"></i>
+                                </Link>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {/*<CourseRow title="CS5610" owner="me"/>*/}
+                    {/*<CourseRow title="CS3200" owner="you"/>*/}
+                    {/*<CourseRow title="CS5200" owner="him"/>*/}
+                    {/*<CourseRow title="CS4550" owner="she"/>*/}
+                    {
+                        this.props.courses.map(course =>
+                            <CourseRow
+                                key={course._id}
+                                deleteCourse={this.props.deleteCourse}
+                                updateCourse={this.props.updateCourse}
+                                course={course}
+                                title={course.title}
+                                lastModified={course.lastModified}
+                                owner={course.owner}/>)
+                    }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
