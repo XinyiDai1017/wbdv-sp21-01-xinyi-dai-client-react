46c9c5c4d0eca2b3c61c63c28e9b20a3aedc9c55
XinyiDai1017 <daixinyi96@gmail.com>
2021-03-07 22:12:55 -0800

    add reducers

---
+++ src/reducers/topic-reducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+    topics: []
+}
+
+const topicReducer = (state=initialState, action) => {
+    switch (action.type) {
+        case "CREATE_TOPIC":
+            return {
+                ...state,
+                topics: [
+                    ...state.topics,
+                    action.topic
+                ]
+            }
+        case "FIND_TOPICS":
+            return {
+                ...state,
+                topics: action.topics
+            }
+        default:
+            return state
+    }
+}
+
+export default topicReducer
